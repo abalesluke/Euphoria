a8647ca7d29ede142652bf2dd0d326e54c180dea
Anikin Luke Abales <47096239+abalesluke@users.noreply.github.com>
2021-03-21 02:35:35 +0800

    Update euphoria.py

--- euphoria.py
+++ euphoria.py
@@ -1,8 +1,28 @@
-import os, sys
+import os, sys, time, re
 from pyngrok import ngrok
 
-ngrok_url = ngrok.connect(6666, 'http')
-print('wait the fuck up..')
-os.system('php -S 127.0.0.1:6666 > /dev/null 2>&1 &')
-sys.stdout.write('\r[+] LINK: ')
-os.system('curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io"')
+
+def check():
+	pattern = r'IP:'
+
+	while True:
+		if(os.path.exists('ip.txt')):
+			f = open('ip.txt', 'r')
+			for ip in f.readlines():
+				if(re.match(pattern, ip)):
+					print(ip)
+					
+					os.system('rm ip.txt')
+				else:
+					pass
+
+
+def server():
+	ngrok_url = ngrok.connect(6666, 'http')
+	os.system('php -S 127.0.0.1:6666 > /dev/null 2>&1 &')
+	sys.stdout.write('\r[+] LINK: ')
+	os.system('curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io"')
+	check()
+
+
+server()
